8399391b34a6389bb849b62a19f300f6c1759578
mallikapps <mallik.techno@gmail.com>
2022-05-16 12:41:14 +0530

    user class is added with three properties

---
+++ MyVCApp/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyVCApp
+{
+    public class User
+    {
+        public int UserId { set; get; }
+        public string UserName { set; get; }
+        public string Password { set; get; }
+
+    }
+}
